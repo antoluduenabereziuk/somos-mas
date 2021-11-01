da8ac34fc1159c55cf8b695934e4484709004bc3
Luciano Ibarra <luciano39340268@gmail.com>
2021-10-31 21:20:17 -0300

    +

--- client/src/routes/AppRouter.js
+++ client/src/routes/AppRouter.js
@@ -1,18 +1,5 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-import { Header } from "../components/Header/Header";
-import { Footer } from "../components/Footer/Footer";
-import { Home } from "../pages/Home";
-import { About } from "../pages/About";
-import { Activities } from "../pages/Activities";
-import { News } from "../pages/News";
-import { Novelties } from "../pages/Novelties";
-import { Testimonials } from "../pages/Testimonials";
-import { Contacts } from "../pages/Contacts";
-import { Contribute } from "../pages/Contribute";
-import { User } from "../pages/User";
-import { NotFound } from "../pages/NotFound";
 import { AuthRouter } from "./AuthRouter";
 import { MainRouter } from "./MainRouter";
 
--- client/src/routes/MainRouter.js
+++ client/src/routes/MainRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router'
+import { Switch, Route } from 'react-router'
 import { Footer } from '../components/Footer/Footer'
 import { Header } from '../components/Header/Header'
 import { About } from '../pages/About'
