8674d84b2eecf5c9f91bcc710b84999eeefdfa07
Luciano Ibarra <luciano39340268@gmail.com>
2021-11-01 13:20:03 -0300

    Commit

--- client/src/routes/AuthRouter.js
+++ client/src/routes/AuthRouter.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { Login } from "../pages/auth/Login";
+import { Register } from "../pages/auth/Register";
+
 export const AuthRouter = () => {
   return (
     <Switch>
-      <Route exact to="/auth/login" component={Login} />
+        
+      <Route exact path="/auth/login" component={Login} />
 
-      <Route exact to="/auth/register" />
+      <Route exact path="/auth/register" component={ Register } />
 
-      <Redirect to="/auth/login" />
     </Switch>
   );
 };
