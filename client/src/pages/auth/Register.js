cf9eec053722ecf81c2afcf106e990bc3213314a
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-11-01 13:24:27 -0300

    just saving

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -2,7 +2,7 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import logo from "../../assets/images/logo.png";
-import './auth.scss';
+import "./auth.scss";
 
 const validationSchema = Yup.object({
   firstName: Yup.string().required("Required"),
@@ -31,7 +31,7 @@ export const Register = () => {
   return (
     <div className="Auth">
       <div className="auth__content">
-        <img src={logo} alt="Somos Mas Logo" /> 
+        <img src={logo} alt="Somos Mas Logo" />
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -41,62 +41,54 @@ export const Register = () => {
             <div className="input-box">
               <label htmlFor="firstName">Nombre</label>
               <Field
-                  type="text"
-                  className="input"
-                  name="firstName"
-                  id="firstName"
-                  required
-                />
+                type="text"
+                className="input"
+                name="firstName"
+                id="firstName"
+                required
+              />
               <ErrorMessage name="firstName">
-                  {(error) => (
-                    <div className="alert">{error}</div>
-                  )}
-                </ErrorMessage>
+                {(error) => <div className="alert">{error}</div>}
+              </ErrorMessage>
             </div>
             <div className="input-box">
               <label htmlFor="lastName">Apellido</label>
               <Field
-                  type="text"
-                  className="input"
-                  name="lastName"
-                  id="lastName"
-                  required
-                />
+                type="text"
+                className="input"
+                name="lastName"
+                id="lastName"
+                required
+              />
               <ErrorMessage name="lastName">
-                  {(error) => (
-                    <div className="alert">{error}</div>
-                  )}
-                </ErrorMessage>
+                {(error) => <div className="alert">{error}</div>}
+              </ErrorMessage>
             </div>
             <div className="input-box">
               <label htmlFor="email">Email</label>
               <Field
-                  type="email"
-                  className="input"
-                  name="email"
-                  id="email"
-                  required
-                />
+                type="email"
+                className="input"
+                name="email"
+                id="email"
+                required
+              />
               <ErrorMessage name="email">
-                  {(error) => (
-                    <div className="alert">{error}</div>
-                  )}
+                {(error) => <div className="alert">{error}</div>}
               </ErrorMessage>
             </div>
             <div className="input-box">
               <label htmlFor="password">Contraseña</label>
               <Field
-                  type="password"
-                  className="input"
-                  name="password"
-                  id="password"
-                  required
+                type="password"
+                className="input"
+                name="password"
+                id="password"
+                required
               />
               <ErrorMessage name="password">
-                  {(error) => (
-                    <div className="alert">{error}</div>
-                  )}
-                </ErrorMessage>
+                {(error) => <div className="alert">{error}</div>}
+              </ErrorMessage>
             </div>
             <div className="input-box">
               <button type="submit" className="btn primary">
